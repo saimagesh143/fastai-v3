7de4a3ca4e45b163505fa11a046973be03d1acc9
saimagesh143 <45652121+saimagesh143@users.noreply.github.com>
2020-03-08 00:06:07 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1-2fL2youQfd-ghCMLYYpsdm28dRMwb4Y'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['acne','ringworm','blister']
 path = Path(__file__).parent
 
 app = Starlette()
